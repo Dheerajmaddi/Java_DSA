e8e9e6b39800832f9edbac81ce77677d03ce22c9
Dheeraj Maddi <dheerajmaddi@gmail.com>
2025-04-01 00:46:17 -0500

    Contains two apooraches for Longest Subtring length calculation using HashMap and HashSet

--- src/SlidingWindowPatterns/LongestSubstringLength.java
+++ src/SlidingWindowPatterns/LongestSubstringLength.java
@@ -1,6 +1,8 @@
 package SlidingWindowPatterns;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class LongestSubstringLength {
@@ -9,7 +11,7 @@ public class LongestSubstringLength {
 
         int left = 0, right = 0, maxSubstringLength = 0;
         Set<Character> slidingWindow = new HashSet<>();
-
+        // Using HashSet
         while(right < str.length()){
             if(slidingWindow.contains(str.charAt(right))){
                 slidingWindow.remove(str.charAt(right));
@@ -21,6 +23,23 @@ public class LongestSubstringLength {
             right++;
         }
 
-        System.out.println("Substring length: " + maxSubstringLength);
+        System.out.println("HashSet approach for Max Substring length: " + maxSubstringLength);
+
+        left = 0;
+        right = 0;
+        maxSubstringLength = 0;
+        Map<Character, Integer> slidingWindowMap = new HashMap<>();
+
+        while(right < str.length()){
+
+            if(slidingWindowMap.containsKey(str.charAt(right))){
+                left = slidingWindowMap.get(str.charAt(right)) + 1;
+            }
+
+            slidingWindowMap.put(str.charAt(right), right);
+            maxSubstringLength = Math.max(maxSubstringLength, right - left + 1);
+            right++;
+        }
+        System.out.println("HashMap approach for Max Substring length: " + maxSubstringLength);
     }
 }
